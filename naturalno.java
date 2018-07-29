24b61fc3c93d21ce8206086c2168d3649303f23f
abikanish <41136963+abikanish@users.noreply.github.com>
2018-07-29 13:58:16 +0530

    Create naturalno.java

---
+++ naturalno.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class naturalno {
+public static void main(String []args){
+	int n,sum=0;
+	Scanner input=new Scanner(System.in);
+	n=input.nextInt();
+	for(int i=1;i<=n;i++){
+		sum=sum+i;
+		}
+	}
+}
